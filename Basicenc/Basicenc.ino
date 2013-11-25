55ff3bc346e35913c2b38b6fbb073e7ad082c964
andzerb <andzerb@nuevaschool.org>
2013-11-25 14:22:08 -0800

    Added enc test

---
+++ Basicenc/Basicenc.ino
@@ -0,0 +1,31 @@
+/* Encoder Library - Basic Example
+- * http://www.pjrc.com/teensy/td_libs_Encoder.html
+ *
+ * This example code is in the public domain.
+ */
+
+#include <Encoder.h>
+
+// Change these two numbers to the pins connected to your encoder.
+//   Best Performance: both pins have interrupt capability
+//   Good Performance: only the first pin has interrupt capability
+//   Low Performance:  neither pin has interrupt capability
+Encoder myEnc(21, 22);
+//   avoid using pins with LEDs attached
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Basic Encoder Test:");
+}
+
+long oldPosition  = -999;
+
+void loop() {
+  analogWrite(2,255);
+
+  long newPosition = myEnc.read();
+  if (newPosition != oldPosition) {
+    oldPosition = newPosition;
+    Serial.println(newPosition);
+  }
+}
