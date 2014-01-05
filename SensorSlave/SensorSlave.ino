ddade00296e0bfb764fcf178c43a6d485380a0e9
andzerb <azrobotwiz@yahoo.com>
2014-01-05 14:50:58 -0800

    Sensor Slave Working
    It works!

--- SensorSlave/SensorSlave.ino
+++ SensorSlave/SensorSlave.ino
@@ -1,40 +1,57 @@
-#include <Encoder.h>
-
-#include <Servo.h>
-
-const int switchPins[] = {
-  26, 27, 28, 29
+const int switchPins[4] = {
+  2, 3, 4, 5
 };
+const int pingPins[2][4] = {
+  {6, 7, 8, 9},
+  {10, 11, 12, 13}  
+};
+const int flamePins[2] = {
+  A0, A1
+};
+const int lightPins[4] = {
+  A2, A3, A4, A5
+};
+
 const boolean xInvert = true;
 const boolean yInvert = true;
 const int flameThreshold = 700;
 
 void setup() {
-  // initialize serial:
-  Serial.begin(230400);
-
-  // reserve 200 bytes for the inputString:
-  inputString.reserve(200);
+  Serial.begin(115200);
 
   for (int i = 0; i < 4; i++) {
     pinMode(switchPins[i], INPUT);
   }
+  for (int i = 0; i < 4; i++) {
+    pinMode(pingPins[0][i], INPUT);
+  }
+  for (int i = 0; i < 4; i++) {
+    pinMode(pingPins[1][i], OUTPUT);
+  }
+  for (int i = 0; i < 2; i++) {
+    pinMode(flamePins[i], INPUT);
+  }
+  for (int i = 0; i < 4; i++) {
+    pinMode(lightPins[i], INPUT);
+  }
+  Serial.println();
 }
 void loop()
 {
+  delay(10);
   Serial.print("Okay "); // If the brain sees anything different than a fully spelled okay, with a capital O and a space, the line will be rejected.
   // Serious stuff goes here!
   Serial.print("F1L"); Serial.print(flameSensor(flamePins[0])); // Start with the first flame sensor ("f"), the one to the left ("l"). The orientation is for debug purposes.
   Serial.print("F2R"); Serial.print(flameSensor(flamePins[1])); // Then print the second flame sensor's reading
   
-  for(int i = 0; i < 4, i++){
-    Serial.println("S"); Serial.print(i + 1); Serial.print(digitalRead(switchPins[i])); // I don't need orientation on any others because the 1 refers to the top as protocol.
+  for(int i = 0; i < 4; i++){
+    Serial.print("S"); Serial.print(i + 1); Serial.print(digitalRead(switchPins[i])); // I don't need orientation on any others because the 1 refers to the top as protocol.
   }
-  for(int i = 0; i < 4, i++){
-    Serial.println("P"); Serial.print(i + 1); Serial.print(SR04(pingPins[i][0])); Serial.print(SR04(pingPins[i][1])); // P is for proximity, the two pins are necessary in SR04 type devices.
+  for(int i = 0; i < 4; i++){
+    Serial.print("P"); Serial.print(i + 1); Serial.print(SR04(pingPins[0][i],pingPins[1][i])); // P is for proximity, the two pins are necessary in SR04 type devices.
   }
-  for(int i = 0; i < 4, i++){
-    Serial.println("L"); Serial.print(i + 1); Serial.print(analogRead(lightPins[i]) * 255/100); // L is for light sensor and units are percentages.
+  for(int i = 0; i < 4; i++){
+    Serial.print("L"); Serial.print(i + 1); Serial.print(analogRead(lightPins[i])); // L is for light sensor and units are percentages.
   }
   //End of serious stuff
   Serial.println(); // A nl is the delimiter for a new "packet", so to speak
@@ -78,18 +95,16 @@ int SR04 (int outPin, int inPin) {
 
   // The PING))) is triggered by a HIGH pulse of 2 or more microseconds.
   // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
-  pinMode(outPin, OUTPUT);
-  digitalWrite(pingPin, LOW);
+  digitalWrite(outPin, LOW);
   delayMicroseconds(2);
-  digitalWrite(pingPin, HIGH);
+  digitalWrite(outPin, HIGH);
   delayMicroseconds(5);
-  digitalWrite(pingPin, LOW);
+  digitalWrite(outPin, LOW);
 
   // The same pin is used to read the signal from the PING))): a HIGH
   // pulse whose duration is the time (in microseconds) from the sending
   // of the ping to the reception of its echo off of an object.
-  pinMode(inPin, INPUT);
-  duration = pulseIn(pingPin, HIGH);
+  duration = pulseIn(inPin, HIGH, 50000);
 
   // convert the time into a distance
   inches = microsecondsToInches(duration);
@@ -114,7 +129,3 @@ long microsecondsToCentimeters(long microseconds)
   // object we take half of the distance travelled.
   return microseconds / 29 / 2;
 }
-double degreetoRad(double Degrees) {
-  return 2 * pi * Degrees / 360;
-}
-
