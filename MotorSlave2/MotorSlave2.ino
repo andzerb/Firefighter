36db68dc109c2dfaeb0d6c69bebf3c673c02e0d1
andzerb <azrobotwiz@yahoo.com>
2014-02-05 14:10:04 -0800

    Github being weird
    Okay it should work now.

--- MotorSlave2/MotorSlave2.ino
+++ MotorSlave2/MotorSlave2.ino
@@ -29,8 +29,8 @@ const boolean yInvert = true;
 boolean moving = false;
 int wheelPowers[] = {0,0,0,0};
 long prevSpeeds[] = {0,0,0,0};
-int xreq = 0;
-int yreq = 0;
+int xreq = 50;
+int yreq = 50;
 int mSpeed = 200;
 
 void setup(){
@@ -48,55 +48,64 @@ void setup(){
   enc2.write(0);
   enc3.write(0);
   enc4.write(0);
-  xreq = 500;
-  yreq = 500;
-  for(int i = 0; i < 4; i++){wheelPowers[i] = 150;}
+  xreq = 10;
+  yreq = 10;
+  for(int i = 0; i < 4; i++){wheelPowers[i] = 255;}
   delay(1000);
  
 }
+
 void loop()
 { 
-  delay(100);
+  
+  delay(25);
+  
   if(!(xreq == 0 && yreq == 0)){        
     long readSpeeds[] ={enc1.read(), enc2.read(), enc3.read(), enc4.read()};
-    float ximbalance = (readSpeeds[0]-prevSpeeds[0] + 0.01)/(readSpeeds[3]-prevSpeeds[3] + 0.01);
-    float yimbalance = (readSpeeds[1]-prevSpeeds[1] + 0.01)/(readSpeeds[4]-prevSpeeds[2] + 0.01);
+    float ximbalance = (readSpeeds[0]-prevSpeeds[0] + 0.01)/(readSpeeds[2]-prevSpeeds[2] + 0.01);
+    float yimbalance = (readSpeeds[1]-prevSpeeds[1] + 0.01)/(readSpeeds[3]-prevSpeeds[3] + 0.01);
     if(wheelPowers[2]*ximbalance <= 255){wheelPowers[2]*= ximbalance;}
-    if(wheelPowers[3]*ximbalance <= 255){wheelPowers[3]*= ximbalance;}
+    if(wheelPowers[3]*yimbalance <= 255){wheelPowers[3]*= yimbalance;}
+    Serial.print(yreq);
+    Serial.print(", ");
+    Serial.print(readSpeeds[0]);
+    Serial.print(", ");
+
+    Serial.println(readSpeeds[1]);
     for(int i = 0; i < 4; i++){
      prevSpeeds[i] = readSpeeds[i]; 
     }
-    Serial.println(readSpeeds[0]);
-    if(readSpeeds[0] >= xreq/mmpercount){
+    if(abs(readSpeeds[0]) >= abs(xreq/mmpercount)){
       motor1->run(RELEASE);
       motor1->setSpeed(0);
     }else{
-      motor1->run(FORWARD);
+      if(xreq>=0){motor1->run(FORWARD);}else{motor1->run(BACKWARD);}
       motor1->setSpeed(wheelPowers[0]); 
     }
-    if(readSpeeds[1] >= yreq/mmpercount){      
+    if(abs(readSpeeds[1]) >= abs(yreq/mmpercount)){      
       motor2->run(RELEASE);
       motor2->setSpeed(0);
     }else{
-      motor2->run(FORWARD);
+      if(yreq>=0){motor2->run(FORWARD);}else{motor2->run(BACKWARD);}
       motor2->setSpeed(wheelPowers[1]); 
     }
-    if(readSpeeds[2] >= xreq/mmpercount){
+    if(abs(readSpeeds[2]) >= abs(xreq/mmpercount)){
       motor3->run(RELEASE);
       motor3->setSpeed(0);
     }else{
-      motor3->run(FORWARD);
+      if(xreq>=0){motor3->run(FORWARD);}else{motor3->run(BACKWARD);}
       motor3->setSpeed(wheelPowers[2]); 
     }
-    if(readSpeeds[3] >= yreq/mmpercount){
+    if((readSpeeds[3]) >= abs(yreq/mmpercount)){
       motor4->run(RELEASE);
       motor4->setSpeed(0);      
     }else{
-      motor4->run(FORWARD);
+      if(yreq>=0){motor4->run(FORWARD);}else{motor4->run(BACKWARD);}
       motor4->setSpeed(wheelPowers[3]); 
     }
     
-    if(readSpeeds[0] >= xreq/mmpercount && readSpeeds[1] >= yreq/mmpercount && readSpeeds[2] >= xreq/mmpercount && readSpeeds[3] >= yreq/mmpercount){
+    
+    if(abs(readSpeeds[0]) >= abs(xreq/mmpercount) && abs(readSpeeds[1]) >= abs(yreq/mmpercount) && abs(readSpeeds[2]) >= abs(xreq/mmpercount) && abs(readSpeeds[3]) >= abs(yreq/mmpercount)){
       motor1->run(RELEASE);
       motor2->run(RELEASE);
       motor3->run(RELEASE);
@@ -110,21 +119,49 @@ void loop()
       enc2.write(0);
       enc3.write(0);
       enc4.write(0);
-    }    
+    }  
   }
-  
+  if (stringComplete) {
+    Serial.println(inputString);
+    String strData = inputString;
+    int nComma = strData.indexOf(',');
+    // Get the characters up to, but not including the comma
+    String xString = strData.substring(0, nComma);
+    // Get the characters after the comma
+    String yString = strData.substring(nComma+1);
+    // clear the string:
+    xreq = xString.toInt();
+    yreq = yString.toInt();
+    enc1.write(0);
+    enc2.write(0);
+    enc3.write(0);
+    enc4.write(0);
+    wheelPowers[0] = 200;
+    wheelPowers[1] = 200;
+    wheelPowers[2] = wheelPowers[0];
+    wheelPowers[3] = wheelPowers[1]; 
+    inputString = "";
+    stringComplete = false;
+   }
 }
-boolean serialEvent() {
+
+/*
+  SerialEvent occurs whenever a new data comes in the
+ hardware serial RX.  This routine is run between each
+ time loop() runs, so using delay inside loop can delay
+ response.  Multiple bytes of data may be available.
+ */
+void serialEvent() {
   while (Serial.available()) {
     // get the new byte:
-    char inChar = (char)Serial.read(); 
+    char inChar = (char)Serial.read();
     // add it to the inputString:
     inputString += inChar;
     // if the incoming character is a newline, set a flag
     // so the main loop can do something about it:
     if (inChar == '\n') {
-      return true;
-    } 
+      stringComplete = true;
+    }
   }
 }
 
